cbacb843e928a557797ce727b41ea2fd680ce438
Võ Phạm Quốc Anh <vophamq123@gmail.com>
2022-08-20 21:24:23 +0700

    server

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mycourse.uqi5o.mongodb.net/?retryWrites=true&w=majority`)
+        console.log('Mongoose connected!')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
+
+const PORT = process.env.PORT  || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+
+})
